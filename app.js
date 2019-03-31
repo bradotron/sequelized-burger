dce59f19e1a53ed47ec4df542ffed099459c4de2
Bradley Jensen <bradley13@gmail.com>
2019-03-30 20:40:39 -0400

    testing sequelize

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var burgersRouter = require(`./routes/burgers`);
-
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,8 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', burgersRouter);
 
 var apiRouter = require(`./routes/apiRoutes.js`)(app);
 
